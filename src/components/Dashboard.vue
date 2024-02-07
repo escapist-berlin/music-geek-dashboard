7ccc4d3f0083797d59fae864cfb81c65931c1527
Denis Kolokolov <dennis.kolokolov@gmail.com>
2024-02-07 19:26:54 +0100

    [ADD] Sidebar

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="dashboard-container">
-    <h1>HELLO</h1>
+    <Sidebar />
+    <div class="widgets-container">
+      <h1>Widgets</h1>
+    </div>
   </div>
 </template>
 
 <script>
+import Sidebar from "../components/Sidebar.vue";
+
+export default {
+  components: {
+    Sidebar,
+  }
+}
 </script>
 
 <style scoped>
@@ -12,11 +22,22 @@
     background-color: rgba(45, 49, 66, 0.95);
     width: 100%;
     height: 100%;
-    padding: 20px;
     border-radius: 15px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     display: flex;
+    flex-direction: row;
+  }
+  .widgets-container {
+    flex-grow: 1;
+    border: 5px solid lightcoral;
+    display: flex;
     justify-content: center;
     align-items: center;
+  }
+
+  @media (max-width: 768px) {
+    .dashboard-container {
+      flex-direction: column;
+    }
   }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sidebar-container">
+    <h1>Sidebar</h1>
+  </div>
+</template>
+
+<style scoped>
+  .sidebar-container {
+    /* background-color: rgba(45, 49, 66, 0.95); */
+    width: 10%;
+    height: 100%;
+    border: 5px solid red;
+    /* border-radius: 15px; */
+    /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media (max-width: 768px) {
+    .sidebar-container {
+      width: 100%;
+      height: 10%;
+    }
+  }
+</style>
