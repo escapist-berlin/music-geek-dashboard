d72e2192e6138d8677d89eaf00a2cb9754aebdbf
Denis Kolokolov <dennis.kolokolov@gmail.com>
2024-02-08 14:52:21 +0100

    [ADD] Sidebar icons

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,26 +1,72 @@
 <template>
   <div class="sidebar-container">
-    <h1>Sidebar</h1>
+    <button class="sidebar-button">
+      <font-awesome-icon icon="fa-solid fa-house" class="sidebar-icon" />
+    </button>
+    <button class="sidebar-button">
+      <font-awesome-icon icon="fa-solid fa-calendar" class="sidebar-icon" />
+    </button>
+    <button class="sidebar-button">
+      <font-awesome-icon icon="fa-solid fa-video" class="sidebar-icon" />
+    </button>
+    <button class="sidebar-button">
+      <font-awesome-icon icon="fa-solid fa-message" class="sidebar-icon" />
+    </button>
+    <button class="sidebar-button">
+      <font-awesome-icon icon="fa-solid fa-gear" class="sidebar-icon" />
+    </button>
   </div>
 </template>
 
+<script>
+</script>
+
 <style scoped>
   .sidebar-container {
-    /* background-color: rgba(45, 49, 66, 0.95); */
-    width: 10%;
-    height: 100%;
-    border: 5px solid red;
+    width: 100%;
+    height: 10%;
+    /* border: 5px solid red; */
     /* border-radius: 15px; */
     /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); */
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: row;
+    gap: 30px;
   }
 
-  @media (max-width: 768px) {
+  .sidebar-button {
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  .sidebar-icon {
+    color: rgba(214, 214, 214, 0.6);
+    font-size: 18px;
+  }
+
+  .sidebar-button:hover .sidebar-icon {
+    color: rgba(214, 214, 214, 1);
+  }
+
+  @media (min-width: 576px) and (max-width: 991px) {
     .sidebar-container {
-      width: 100%;
-      height: 10%;
+      gap: 50px;
+    }
+    .sidebar-icon {
+      font-size: 21px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .sidebar-container {
+      width: 10%;
+      height: 100%;
+      flex-direction: column;
+      gap: 70px;
+    }
+    .sidebar-icon {
+      font-size: 24px;
     }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHouse, faCalendar, faVideo, faMessage, faGear } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 const app = createApp(App)
 
 app.use(createPinia())
+
+library.add(faHouse, faCalendar, faVideo, faMessage, faGear);
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app')
