df6aaac850d8e259ff6e318a097b9c2a9b9214bd
Denis Kolokolov <dennis.kolokolov@gmail.com>
2024-02-11 19:18:40 +0100

    [WIP] MixCloudWidget: cover & description

--- src/components/widgets/MixCloudWidget.vue
+++ src/components/widgets/MixCloudWidget.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="mixcloud-container">
-    <div v-if="show">
-      <a :href="show.url"><img :src="show.pictures.large" alt="Release Cover" style="width: 150px; height: 150px;"></a>
-      <h1>{{ show.name }}</h1>
-      <p>{{ show.description }}</p>
+  <div v-if="show && comments" class="mixcloud-container">
+    <h1>{{ show.name }}</h1>
+    <div class="show-cover-infos">
+      <div>
+        <br>
+        <p>{{ getDescription(show.description) }}</p>
+        <br>
+        <a v-if="show.description.includes('www')" :href="extractUrl(show.description)">
+          <font-awesome-icon icon="fa-solid fa-angles-right" fade />
+          Read more
+        </a>
+      </div>
+      <a :href="show.url">
+        <img :src="show.pictures.large" alt="Show Cover" style="width: 90px; height: 90px;">
+      </a>
     </div>
-    <div v-if="comments && show">
+
+    <div class="show-comments">
       <h2>Comments ({{ show.comment_count }}):</h2>
       <ul class="comment-list">
         <li v-for="(comment, index) in comments.data.slice(0, 3)" :key="index" class="comment-item">
@@ -49,17 +60,51 @@ export default {
         throw new Error('Failed to fetch release data');
       }
     },
+    getDescription(description) {
+      // Extracting the part before "Read more:"
+      const endIndex = description.indexOf("Read");
+      return endIndex > -1 ? description.substring(0, endIndex) : description;
+    },
+    extractUrl(description) {
+      // Extract the URL from the description
+      const urlPattern = /(?:https?:\/\/)?(?:www\.)?[\w\.-]+\.\w{2,}(?:\/\S*)?/;
+      const urlMatch = description.match(urlPattern);
+      return urlMatch ? (urlMatch[0].startsWith("www.") ? "https://" + urlMatch[0] : urlMatch[0]) : null;
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
   .mixcloud-container {
-    border: 5px solid lightblue;
     display: flex;
-    justify-content: center;
-    align-items: center;
+    gap: 10px;
     flex-direction: column;
+
+    padding: 20px;
+    border-radius: 10px;
+    width: 300px;
+    height: 300px;
+    font-size: 15px;
+
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.7);
+    border: 1px solid #ccc;
+    background-image: linear-gradient(to bottom right, #AE66BF, #267FBF);
+
+    h1 {
+      margin-left: auto;
+      margin-right: auto;
+      font-size: 19px;
+    }
+
+    .show-cover-infos {
+      display: flex;
+      gap: 10px;
+    }
+
+    .show-comments {
+
+    }
   }
 
   @media (min-width: 768px) {
--- src/main.js
+++ src/main.js
@@ -6,14 +6,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHouse, faCalendar, faVideo, faMessage, faGear, faCloud, faCloudRain, faWind, faSun, faCompactDisc } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faCalendar, faVideo, faMessage, faGear, faCloud, faCloudRain, faWind, faSun, faCompactDisc, faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 const app = createApp(App)
 
 app.use(createPinia())
 
-library.add(faHouse, faCalendar, faVideo, faMessage, faGear, faCloud, faCloudRain, faWind, faSun, faCompactDisc);
+library.add(faHouse, faCalendar, faVideo, faMessage, faGear, faCloud, faCloudRain, faWind, faSun, faCompactDisc, faAnglesRight);
 app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app')
